74dabf3e95d6bc37a6dfa388039f4dab6c60de06
Tim Savage <tim.savage@gmail.com>
2018-12-15 00:54:54 -0600

    minor formatting

--- Saguaro/src/main/java/com/winterhaven_mc/saguaro/TelnetServer.java
+++ Saguaro/src/main/java/com/winterhaven_mc/saguaro/TelnetServer.java
@@ -24,6 +24,7 @@ import java.io.OutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+
 /**
  * Simple TCP server. Waits for connections on a TCP port in a separate thread.
  * 
